4f12691fb4779142e92c1db90cd61686ae14e417
ayanchavand <ayan.chavand15@gmail.com>
2024-10-10 19:45:46 +0530

    normals are rendered yayy

--- ray tracing lol.cpp
+++ ray tracing lol.cpp
@@ -4,32 +4,32 @@
 
 #include <iostream>
 
-bool hit_sphere(const point3& center, double radius, const ray& r) {
-	vec3 oc = center - r.origin();
-	auto a = dot(r.direction(), r.direction());
-	auto b = 2 * dot(r.direction(), oc);
-	auto c = dot(oc, oc) - (radius * radius);
+double hit_sphere(const point3& center, double radius, const ray& r) {
+    vec3 oc = center - r.origin();
+    auto a = dot(r.direction(), r.direction());
+    auto b = -2.0 * dot(r.direction(), oc);
+    auto c = dot(oc, oc) - radius*radius;
+    auto discriminant = b*b - 4*a*c;
 
-	auto discriminat = b * b - 4 * a * c;
-	return (discriminat >= 0);
+    if (discriminant < 0) {
+        return -1.0;
+    } else {
+        return (-b - std::sqrt(discriminant) ) / (2.0*a);
+    }
 }
 
 color ray_color(const ray& r) {
+    auto t = hit_sphere(point3(0,0,-1), 0.5, r);
+    if (t > 0.0) {
+		//normal calc for sphere
+        vec3 N = unit_vector(r.at(t) - vec3(0,0,-1));
+        return 0.5*color(N.x()+1, N.y()+1, N.z()+1);
+    }
 
-
-	if (hit_sphere(point3(0, 0, -1), 0.5, r)) {
-		return color(1,0, 0);
-	}
-	
-	vec3 unit_direction = unit_vector(r.direction());
-	// a = (y + 1) / 2
-	double a = 0.5 * (unit_direction.y() + 1);
-	//blendedValue = (1−a)⋅startValue + a⋅endValue
-	//This is basically the formula of lerp function
-	color pixel_color = (1 - a) * color(1, 1, 1) + a * color(0, 0, 1);
-	return pixel_color;
+    vec3 unit_direction = unit_vector(r.direction());
+    auto a = 0.5*(unit_direction.y() + 1.0);
+    return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0);
 }
-
 int main() {
 	
 	//Create the image file
