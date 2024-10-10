6ea376af8f0e9ad5e992e79533a02637500bcb84
ayanchavand <ayan.chavand15@gmail.com>
2024-10-10 19:49:00 +0530

    made the at function for consts

--- ray.h
+++ ray.h
@@ -10,7 +10,7 @@ public:
 	const point3& origin() const { return orig; }
 	const vec3& direction() const { return dir; }
 
-	point3 at(double t) {
+	point3 at(double t)const {
 		return orig + t * dir;
 	}
 private:
